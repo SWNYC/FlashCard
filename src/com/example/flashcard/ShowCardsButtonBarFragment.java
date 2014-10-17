503106affe06e62f9243a38160cec778c684ae65
SWNYC <stephenwongnyc@gmail.com>
2014-10-16 22:22:29 -0400

    Changed UI for creating cards, bug fixes

--- src/com/example/flashcard/ShowCardsButtonBarFragment.java
+++ src/com/example/flashcard/ShowCardsButtonBarFragment.java
@@ -4,21 +4,30 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class ShowCardsButtonBarFragment extends Fragment {
 	private Button showAnswerButton;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflator, ViewGroup parent,
 			Bundle savedInstanceState) {
 
-		View v = inflator
-				.inflate(R.layout.show_cards_button_bar_fragment, parent, false);
+		View v = inflator.inflate(R.layout.show_cards_button_bar_fragment,
+				parent, false);
 
 		showAnswerButton = (Button) v.findViewById(R.id.showAnswerButton);
-		
+		showAnswerButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 		return v;
 	}
 }
--- src/com/example/flashcard/ShowCardsFragment.java
+++ src/com/example/flashcard/ShowCardsFragment.java
@@ -6,15 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class ShowCardsFragment extends Fragment {
-	private static final String TAG = "SHOWCARDS";
 	private TextView mTextView;
-	private Button mShowAnswerButton;
 	public static final String EXTRA_CARD_ID = "com.example.flashcards.card_id";
 	public static final String EXTRA_CARD_DATABASE = "com.example.flashcards.card_database";
 	private FlashCard card;
@@ -22,34 +18,35 @@ public class ShowCardsFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		UUID cardID = (UUID)getArguments().getSerializable(EXTRA_CARD_ID);
-		FlashCardDatabase mCardDatabase = (FlashCardDatabase)getArguments().getSerializable(EXTRA_CARD_DATABASE);
-		
+		UUID cardID = (UUID) getArguments().getSerializable(EXTRA_CARD_ID);
+		FlashCardDatabase mCardDatabase = (FlashCardDatabase) getArguments()
+				.getSerializable(EXTRA_CARD_DATABASE);
+
 		card = mCardDatabase.getCard(cardID);
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflator, ViewGroup parent,
 			Bundle savedInstanceState) {
 
-		View v = inflator
-				.inflate(R.layout.card_fragment, parent, false);
+		View v = inflator.inflate(R.layout.card_fragment, parent, false);
 
 		mTextView = (TextView) v.findViewById(R.id.textView);
 		mTextView.setText(card.getQuestion());
-		
+
 		return v;
 	}
-	
-	public static ShowCardsFragment newInstance(UUID crimeID, FlashCardDatabase database) {
+
+	public static ShowCardsFragment newInstance(UUID crimeID,
+			FlashCardDatabase database) {
 		Bundle args = new Bundle();
 		args.putSerializable(EXTRA_CARD_ID, crimeID);
 		args.putSerializable(EXTRA_CARD_DATABASE, database);
-		
+
 		ShowCardsFragment fragment = new ShowCardsFragment();
 		fragment.setArguments(args);
-		
+
 		return fragment;
 	}
-	
+
 }
